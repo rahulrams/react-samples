599ea7defee0d409d31035ac402d4ad559aa4dd8
rahulrams <rahulrams@gmail.com>
2026-03-31 12:06:28 +0530

    minor base code error fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
-
 import './index.css';
 import App from './App.tsx';
 
